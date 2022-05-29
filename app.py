3dda4034ce9a53473056b56afe464a6f0aac7397
Ray Yoon <jaehoonyoon222@gmail.com>
2022-05-29 15:56:31 -0700

    Updating files

--- app.py
+++ app.py
@@ -9,8 +9,38 @@ app = Flask(__name__)
 def homepage():
     return(
         f'Welcome!<br/>'
-        f"Available Routes:<br/>"
+        f'Available Routes:<br/>'
+        f'/api/v1.0/precipitation'
+        f'/api/v1.0/stations'
+        f'/api/v1.0/tobs'
+        f'/api/v1.0/<start>'
+        f'/api/v1.0/<start>/<end>'
     )
+
+@app.route('/api/v1.0/precipitation')
+def precipitation():
+    return jsonify()
+    #return query results of 'date' and 'prcp' as dict
+    #jsonify
+
+
+@app.route('/api/v1.0/stations')
+def stations():
+    return jsonify()
+    #return list of stations
+    #jsonify
+
+
+@app.route('/api/v1.0/tobs')
+def temperature():
+    return jsonify()
+    #return query results of 'date' and 'tobs' ast dict
+    #jsonify
+
+
+@app.route('/api/v1.0/<start>')
+@app.route('/api/v1.0/<start>/<end>')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
